cbc3729935d72bfee13ea3474d84f6fb33e431b9
jomarca11 <66425146+jomarca11@users.noreply.github.com>
2024-05-02 10:44:11 +0200

    Update streamlit_app.py
    #New Section to display fruitivice nutrition information
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
    st.text(fruityvice_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(":cup_with_straw: Example Streamlit App :cup_with_straw:")
 st.write(
@@ -47,3 +47,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success ("Your Smoothie is ordered")
+#New Section to display fruitivice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
